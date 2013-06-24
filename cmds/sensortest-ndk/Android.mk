ee83aa67e5922cd627d92b574c273543fb9d55e4
Dmitriy Cherkasov <@>
2013-06-24 12:03:20 -0700

    cmds: add sensortest-ndk

---
+++ cmds/sensortest-ndk/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= \
+	sensortest_main.cpp
+LOCAL_MODULE:= sensortest-ndk
+LOCAL_MODULE_TAGS:=optional
+
+LOCAL_SHARED_LIBRARIES := libcutils libbinder libutils libgui libandroid_runtime libandroid
+
+include $(BUILD_EXECUTABLE)
+
+# Normally optional modules are not installed unless they show
+# up in the PRODUCT_PACKAGES list
+
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT)/bin/sensortest
+
---
+++ cmds/sensortest-ndk/sensortest_main.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <android/sensor.h>
+#include <android/looper.h>
+
+
+ASensorEventQueue* sensorEventQueue;
+void* sensor_data;
+
+static int get_sensor_events(int fd, int events, void* data) {
+  ASensorEvent event;
+
+  printf("Callback. fd: %d, events %d\n", fd, events);
+
+  while (ASensorEventQueue_getEvents(sensorEventQueue, &event, 1) > 0) {
+    if(event.type==ASENSOR_TYPE_ACCELEROMETER) {
+            printf("accel(x,y,z,t): %f %f %f %lld\n",
+                 event.acceleration.x, event.acceleration.y,
+                 event.acceleration.z, event.timestamp);
+    }
+  }
+  /* return 1 to continue getting callbacks */
+  return 1;
+}
+
+
+
+int main()
+{
+	ASensorManager* sensorManager = NULL;
+	const ASensor* accelerometerSensor = NULL;
+	ASensorList* sensorList = NULL;
+	ALooper* looper = NULL;
+
+
+	if((looper = ALooper_prepare(0)) == NULL)
+			printf("Looper NULL\n");
+
+	if((sensorManager = ASensorManager_getInstance()) == NULL)
+		printf("sensorManager NULL\n");
+
+	if((accelerometerSensor = ASensorManager_getDefaultSensor(sensorManager, ASENSOR_TYPE_ACCELEROMETER)) == NULL)
+		printf("accelerometerSensor NULL\n");
+
+	/* LOOPER_ID_USER = 3 */
+	if((sensorEventQueue = ASensorManager_createEventQueue(sensorManager,
+	            looper, 3, get_sensor_events, sensor_data)) == NULL)
+		printf("sensorEventQueue NULL\n");
+
+	int ret = ASensorEventQueue_enableSensor(sensorEventQueue,
+	                        accelerometerSensor);
+	printf("enableSensor ret: %d\n", ret);
+
+	int rate = ASensor_getMinDelay(accelerometerSensor) * 100;
+	printf("poll rate: %d usec\n", rate);
+    
+	ret = ASensorEventQueue_setEventRate(sensorEventQueue,
+	                        accelerometerSensor, rate);
+	printf("setEventRate ret: %d\n", ret);
+
+
+	int ident = 0;
+	int events = 0;
+	int fd = 0;
+
+	printf("begin polling\n");
+	ident = ALooper_pollAll(-1, &fd, &events, &sensor_data);
+
+	return 0;
+}
